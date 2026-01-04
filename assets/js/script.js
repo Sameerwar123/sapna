d4f86f0243033761c3c87b245bd04e6642c8e0d4
sameer <sameeramd900@gmail.com>
2026-01-04 13:50:54 +0530

    first cmm

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function showMessage() {
+    let messageElement = document.getElementById("message");
+    messageElement.textContent = "Button clicked! JavaScript is working 🎉";
+    messageElement.style.color = "#333";
+}
